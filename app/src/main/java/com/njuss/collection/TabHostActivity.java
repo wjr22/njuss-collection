60c193a46c51f92d600b5acddd581b6e891fe46e
coqauvin511w <52847721+coqauvin511w@users.noreply.github.com>
2019-07-18 22:32:31 +0800

    已完成和未完成列表

--- app/src/main/java/com/njuss/collection/TabHostActivity.java
+++ app/src/main/java/com/njuss/collection/TabHostActivity.java
@@ -1,25 +1,33 @@
 package com.njuss.collection;
 
+import android.app.TabActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.widget.TabHost;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class TabHostActivity extends AppCompatActivity {
+public class TabHostActivity extends TabActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        TabHost host = getTabHost();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_m_tabhost_);
         TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);
         tabHost.setup();
         LayoutInflater inflater = LayoutInflater.from(this);
+
+
+
         inflater.inflate(R.layout.tab1,tabHost.getTabContentView());
         inflater.inflate(R.layout.tab2,tabHost.getTabContentView());
 
-        tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("已完成").setContent(R.id.left));
-        tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("未完成").setContent(R.id.right));
 
-    }
+        Intent intent1 = new Intent();
+        intent1.setClass(this,ListViewActivity.class);
+
+
+        tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("未收集").setContent(intent1));
+        tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("已收集").setContent(intent1));}
 }
